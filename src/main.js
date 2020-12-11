85653e6a4abb46dfa6e1331c341fef470a1cc4c9
Natalia <nflores.teoria@gmail.com>
2020-12-10 22:41:13 -0300

    Agregando estructura base del proyecto con las rutas y bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/router';
+import store from './store/store';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
--- src/router/router.js
+++ src/router/router.js
@@ -10,13 +10,18 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  // {
+  //   path: '/',
+  //   redirect: '/login'
+  // },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '*',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   }
 ]
 
