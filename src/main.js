d4b1ebd576984c147d170be745293d3dca43261d
Natalia <nflores.teoria@gmail.com>
2020-12-11 14:31:19 -0300

    Firebase instalado y configurado

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/router';
 import store from './store/store';
+import firebase from 'firebase';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { firebaseConfig } from "./config/firebaseConfig";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false;
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="container">
+  <div class="container my-5">
+    <h2>Login</h2>
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
       <b-form-group id="input-group-1" label="Correo electrónico:" label-for="input-1" description="We'll never share your email with anyone else.">
         <b-form-input id="input-1" v-model="form.email" type="email" required placeholder="Enter email"></b-form-input>
