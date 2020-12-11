f6df003b1c283c0b72bf126ba7e49160748042f9
Natalia <nflores.teoria@gmail.com>
2020-12-11 15:08:51 -0300

    Empezando a agregar método de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container my-5">
     <h2>Login</h2>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+    <b-form @submit.prevent="login" @reset="onReset" v-if="show">
       <b-form-group id="input-group-1" label="Correo electrónico:" label-for="input-1" description="We'll never share your email with anyone else.">
         <b-form-input id="input-1" v-model="form.email" type="email" required placeholder="Enter email"></b-form-input>
       </b-form-group>
       <b-form-group id="input-group-2" label="Contraseña:" label-for="input-2">
         <b-form-input id="input-2" v-model="form.password" type="password" required placeholder="Ingrese contraseña"></b-form-input>
       </b-form-group>
-      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="submit" variant="primary">Entrar</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
   </div>
@@ -26,21 +26,30 @@
       }
     },
     methods: {
-      onSubmit(evt) {
-        evt.preventDefault()
-        alert(JSON.stringify(this.form))
-      },
-      onReset(evt) {
-        evt.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
+      login() {
+        if(this.form.email && this.form.password) {
+          console.log('Está entrando');
+        } else {
+          console.log('No has ingresado bien');
+        }
       }
-    }
+    },
+    // methods: {
+    //   onSubmit(evt) {
+    //     evt.preventDefault()
+    //     alert(JSON.stringify(this.form))
+    //   },
+    //   onReset(evt) {
+    //     evt.preventDefault()
+    //     // Reset our form values
+    //     this.form.email = ''
+    //     this.form.name = ''
+    //     // Trick to reset/clear native browser form validation state
+    //     this.show = false
+    //     this.$nextTick(() => {
+    //       this.show = true
+    //     })
+    //   }
+    // }
   }
 </script>
