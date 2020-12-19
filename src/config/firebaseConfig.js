5ab3e6992812113792654622888354174f4491aa
Natalia <nflores.teoria@gmail.com>
2020-12-19 18:13:12 -0300

    Comenté el gitignore del firebaseConfig, el usuario y clave están en el readme

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+export let firebaseConfig = {
+    apiKey: "AIzaSyCB483jP1vQGZ4Ams7HxAlyXDl0r_xeM-c",
+    authDomain: "dl-autenticacion.firebaseapp.com",
+    projectId: "dl-autenticacion",
+    storageBucket: "dl-autenticacion.appspot.com",
+    messagingSenderId: "366282767446",
+    appId: "1:366282767446:web:ab3f23757d29bc28d18ff7"
+};
